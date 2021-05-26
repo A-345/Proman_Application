2a9fb9eb5b2a289e1d57855f079ff7798a186a06
Anchal Jha <anchaljha604@gmail.com>
2021-05-26 06:46:01 +0530

    Completed Proman Application

---
+++ proman-api/src/main/java/com/upgrad/proman/api/exception/RestExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.upgrad.proman.api.exception;
+
+import com.upgrad.proman.api.model.ErrorResponse;
+import com.upgrad.proman.service.exception.AuthenticationFailedException;
+import com.upgrad.proman.service.exception.ResourceNotFoundException;
+import com.upgrad.proman.service.exception.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> resourceNotFoundException(ResourceNotFoundException exe, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<ErrorResponse> authenticationFailedException(AuthenticationFailedException exe, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.UNAUTHORIZED
+        );
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> unauthorizedException(UnauthorizedException exe, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.UNAUTHORIZED
+        );
+    }
+}
---
+++ proman-api/target/generated-sources/io/swagger/api/ApiException.java
@@ -0,0 +1,11 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-05-26T06:39:57.588+05:30")
+
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
